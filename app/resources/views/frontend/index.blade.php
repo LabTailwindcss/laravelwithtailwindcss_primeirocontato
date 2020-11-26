48166e57d65033efa4d61f86859d05ed8ffad941
pwcodigo <pwcodigo@gmail.com>
2020-11-26 13:03:47 -0300

    feat: Criado tela inicial parte 1

--- app/resources/views/frontend/index.blade.php
+++ app/resources/views/frontend/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.master')
 
+@section('title','Login')
+
 @section('content')
-    <div class="bg-blue-500 py-2">
-        sdsd
+    <div class="">
+        <img src="{{asset('assets/img/background.png')}}" alt="imagem-fundo" class="max-h-screen float-right">
+    </div>
+    <div class="py-16 ml-32">
+        <h3 class="text-6xl font-black">Ipiweb</h3>
+    </div>
+    <div class="pt-32 ml-32">
+        <h4 class="text-5xl font-bold"> Formulário de login </h4>
+        <p class="font-semibold">Lorem Ipsum is simply dummy text of the printing.</p>
+    </div>
+    <div class="pt-10 ml-32">
+        <button class="bg-blue-500 w-48 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Registrar</button>
+        <button class="bg-gray-500 w-48 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Entrar</button>
     </div>
 @endsection
--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="1">
     <link href="{{ asset('/css/app.css')}}" rel="stylesheet">
-    <title>Site</title>
+    <title>@yield('title')</title>
 </head>
 <body>
 
-@yield('content')
+    @yield('content')
 
 </body>
 </html>
